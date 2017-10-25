488119e8d26fd62be77e542bbdd9a16b0278b581
Omer Demirel <omer.demirel@mirai-solutions.com>
2017-10-25 15:12:37 +0200

    signature fix (still not compiling)

--- src/main/scala/com/miraisolutions/spark/bigquery/BigQuerySourceRelation.scala
+++ src/main/scala/com/miraisolutions/spark/bigquery/BigQuerySourceRelation.scala
@@ -2,8 +2,9 @@ package com.miraisolutions.spark.bigquery
 
 import com.google.api.services.bigquery.model.TableReference
 import com.spotify.spark.bigquery._
+import org.apache.spark.Partition
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.execution.datasources.jdbc.JDBCRDD
+import org.apache.spark.sql.execution.datasources.jdbc.{JDBCOptions, JDBCRDD}
 import org.apache.spark.sql.sources.{BaseRelation, Filter, PrunedFilteredScan}
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{Row, SQLContext}
@@ -14,7 +15,12 @@ import org.apache.spark.sql.{Row, SQLContext}
  * @param tableRef BigQuery table reference
  * @param sqlContext Spark SQL context
 */
-private[bigquery] case class BigQuerySourceRelation(tableRef: TableReference, sqlContext: SQLContext) extends BaseRelation with PrunedFilteredScan {
+private[bigquery] case class BigQuerySourceRelation(
+    tableRef: TableReference,
+    sqlContext: SQLContext,
+    parts: Array[Partition],
+    jdbcOptions: JDBCOptions)
+  extends BaseRelation with PrunedFilteredScan {
 
   private lazy val table = sqlContext.bigQueryTable(tableRef)
 
